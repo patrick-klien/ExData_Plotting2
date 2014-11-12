f5f2063b47703cfbb3b1fbdad567a73dd4b38730
Patrick Klien <patrick@klien.net>
2014-11-12 13:17:24 -0200

    addign R code for first plot

---
+++ plot1.R
@@ -0,0 +1,13 @@
+## This script will result in a plot to answer question 1 for project 2 of the Exploratory Data Analysis course of Coursera.
+
+## This script assumes that there is a directory called "exdata-data-NEI_data" with the data in it.
+
+plot1 <- function(){
+  ## Read the data into R
+  data <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+  
+  ## Sum up all emissions by year and save into a second variable
+  year_sum <- aggregate(data$Emissions, by = list(data$year), FUN = sum)
+  
+  with(year_sum, plot(Group.1, x, type = "b", ylab = expression(PM[2.5]), lwd = 4, xlab = "Years", main = expression("Total emissions of PM"[2.5])))
+}
